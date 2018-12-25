771f33ad1a938e5f263da1e94ca8284dbd42fd66
dingxiaolin <dxlani001@163.com>
2018-12-25 14:05:59 +0800

    demo

--- index.js
+++ index.js
@@ -12,30 +12,14 @@
             var data=[];
             var time=[];
             $scope.list.forEach(item => {
-                data.push(item[6]);
+                data.push(item[1]);
                 option.series[0].data=data;//给Y轴
                 time.push(new Date(item[0]).toLocaleTimeString());
                 option.xAxis.data=time;//给X轴 插入时间数据
             });
-               
-
               }, function errorCallback(response) {
                   // 请求失败执行代码
           });
-        }, 2000);
-
-        //   setInterval(function () {
-        //     var tem = 20 + Math.random().toFixed(2) * 10;
-        //     tem = tem.toFixed(2); // 随机数模拟温度，只保留两位小数
-        //     var time = new Date();
-        //     option.xAxis.data.push(time.getMinutes()+':'+time.getSeconds());//给X轴 插入时间数据
-        //     option.series[0].data.push(tem);//给Y轴 插入温度数据
-      
-        //     // 如果数据超过30个，把第一个数据删除。
-        //     if(option.xAxis.data.length > 30){
-        //       option.xAxis.data.shift()
-        //       option.series[0].data.shift()
-        //     }
-        //     myChart.setOption(option);
-        //   }, 1000);
+          myChart.setOption(option);
+        }, 10000);
       });
